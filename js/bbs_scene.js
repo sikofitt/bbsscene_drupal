8aa1d731b545851a28000250a4b73ec6824f3401
sk-5 <sikofitt@gmail.com>
2013-06-19 10:59:31 -0700

    Fixed no image to display only for the block.  Before it would show for any broken image. 	modified:   js/bbs_scene.js

--- js/bbs_scene.js
+++ js/bbs_scene.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-  $('img').one('error', function () {
+  $('#block-bbs_scene-bbs_scene_random_png img').one('error', function () {
   
     $(this).attr('src', Drupal.settings.bbs_scene.path + 'images/noimg.png').css('height', '160px');
   
