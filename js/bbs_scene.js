bf16b5fe8fb636d5bc43727cf6f16dc0fe2f1d9a
sk-5 <sikofitt@gmail.com>
2013-07-04 11:00:42 -0700

    Fixed the id so noimg.png shows up again if there is an error displaying the image from bbs-scene.org 	modified:   bbs_scene.module 	modified:   js/bbs_scene.js

--- js/bbs_scene.js
+++ js/bbs_scene.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-  $('#block-bbs_scene-bbs_scene_random_png img').one('error', function () {
+  $('#block-bbs-scene-bbs-scene-random-png img').one('error', function () {
   
     $(this).attr('src', Drupal.settings.bbs_scene.path + 'images/noimg.png').css('height', '160px');
   
